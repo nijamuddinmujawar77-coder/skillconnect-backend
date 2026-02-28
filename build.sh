2225cf66430e4b0c92626d6e58423ee5ebc0daaa
Nijamuddin Mujawar <nijamuddinmujawar77@gmail.com>
2026-02-28 23:01:07 +0530

    Fix: add DISABLE_COLLECTSTATIC=1 to stop buildpack auto-collectstatic on DO

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 python manage.py migrate
 python create_admin.py
 python add_jobs.py
